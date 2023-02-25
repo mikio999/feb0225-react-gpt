fae41d24b53b159aa2ad971ebc3a301484784647
minkyung_kwak <ellen10min@gmail.com>
2023-02-25 16:38:58 +0900

    Modify: goodDay.js 수정

--- src/pages/goodDay.js
+++ src/pages/goodDay.js
@@ -1,35 +1,32 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-export default function goodDay() {
-  const [userInput, setUserInput] = useState("")
-  const [result, setResult] = useState("")
+export default function GoodDay() {
+  const [userInput, setUserInput] = useState("");
+  const [result, setResult] = useState("");
 
   useEffect(() => {
-    console.log(userInput)
-  }, [userInput])
+    console.log(userInput);
+  }, [userInput]);
 
   const submit = async () => {
-    const url = "/api/hello"
+    const url = "/api/hello";
     const res = await axios.post(url, {
-      userInput: userInput
-    })
+      userInput: userInput,
+    });
 
-    console.log(res.data.result)
+    console.log(res.data.result);
 
-    setResult(res.data.result)
-    return true
-  }
+    setResult(res.data.result);
+    return true;
+  };
 
-  return <div>
-    <input 
-    onChange={e => setUserInput(e.target.value)}></input>
-    <button
-      onClick={e => submit()}
-    >Send</button>
-
+  return (
     <div>
-      {result}
+      <input onChange={(e) => setUserInput(e.target.value)}></input>
+      <button onClick={(e) => submit()}>Send</button>
+
+      <div>{result}</div>
     </div>
-  </div>
+  );
 }
